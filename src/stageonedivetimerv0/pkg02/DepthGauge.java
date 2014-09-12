21b961e582d72e73a412553f6d67dc9e1f1a3e9e
Dave Robinson <daverobinson80@gmail.com>
2014-09-12 12:35:32 +0100

    removed fake depth array

--- src/stageonedivetimerv0/pkg02/DepthGauge.java
+++ src/stageonedivetimerv0/pkg02/DepthGauge.java
@@ -12,17 +12,11 @@ package stageonedivetimerv0.pkg02;
 public class DepthGauge {
     //Variable declarations
     double currentDepthFromSensor, previousDepth, currentDepth, depthDifferential, doubleRate;
-    int arrayIndex = 0;
-    String velocity;
-    int rate, cRate, pRate;
-    Thread myThread;
-    //This array is some fake data that would be collected from the depth sensor.
-    final double[] depthArray1 = {3.00, 5.00, 7.00, 10.00, 12.00, 15.00, 14.00, 12.00, 11.00, 8.00, 3.00};
     
     public double getCurrentDepth() {
         //sample the depth sensor and write the value to double currentDepth.
         //Fake depth descender for testing purposes.
-        currentDepthFromSensor = depthArray1[arrayIndex];
+        currentDepthFromSensor = 5;
         //System.out.println(currentDepthFromSensor);
         return currentDepthFromSensor;
     }
