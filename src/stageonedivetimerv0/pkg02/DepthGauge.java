6d28f80ecfe1c818a57539c35638b300bc16d075
Dave Robinson <daverobinson80@gmail.com>
2014-09-12 13:18:18 +0100

    Calculate depth based on value from Sensor

--- src/stageonedivetimerv0/pkg02/DepthGauge.java
+++ src/stageonedivetimerv0/pkg02/DepthGauge.java
@@ -5,19 +5,27 @@
  */
 package stageonedivetimerv0.pkg02;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author dave
  */
 public class DepthGauge {
     //Variable declarations
-    double currentDepthFromSensor, previousDepth, currentDepth, depthDifferential, doubleRate;
-    
+    double previousDepth, depthDifferential, doubleRate, currentDepth;
+    double currentSensorValue = 512;
+    double sensorMaxVal = 1023;
+    double sensorMaxMilliBar = 1400;
+    double sensorMinMilliBar = 100;
+    double sensorResolution = (sensorMaxVal / (sensorMaxMilliBar - sensorMinMilliBar));;
+
     public double getCurrentDepth() {
         //sample the depth sensor and write the value to double currentDepth.
-        //Fake depth descender for testing purposes.
-        currentDepthFromSensor = 5;
+        //Divide by 10 to convert to m.
+        DecimalFormat df = new DecimalFormat("#.00");
+        currentDepth = (currentSensorValue / sensorResolution)/10;
         //System.out.println(currentDepthFromSensor);
-        return currentDepthFromSensor;
+        return Double.parseDouble(df.format(currentDepth));
     }
 }
